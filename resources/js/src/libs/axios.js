3e77548497dc5eab54db95d82770e007037bc9c2
sachin.maurya <sachin.maurya@bizinso.com>
2024-01-01 21:36:40 +0530

    axios header timeout

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -7,9 +7,9 @@ const axiosIns = axios.create({
   // You can add your headers here
   // ================================
   baseURL: window.location.origin + '/api/',
-  timeout: 1000,
+  // timeout: 1000,
   headers: {
-    'X-Custom-Header': 'foobar',
+    // 'X-Custom-Header': 'foobar',
     "Content-Type": 'application/json',
     "Accept": "application/json",
     "Access-Control-Allow-Origin": "*",
